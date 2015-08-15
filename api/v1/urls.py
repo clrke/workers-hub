89ed9168437fef1e44ddc741d3272b225014d5fc
Prince John Lansang <enprixd@gmail.com>
2015-08-15 20:25:28 +0800

    Add workers url

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^login', api.v1.views.login),
     url(r'^request', api.v1.views.create_request),
     url(r'^proposal', api.v1.views.create_proposal),
+    url(r'^workers/(?P<id>\d+)', api.v1.views.show_worker),
 ]
