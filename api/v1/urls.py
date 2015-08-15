0f25834565fb629c185622954a1f6ef105178e09
Prince John Lansang <enprixd@gmail.com>
2015-08-15 23:23:13 +0800

    add GET method for proposal url

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,8 +4,8 @@ import api.v1.views
 urlpatterns = [
     url(r'^login$', api.v1.views.login),
     url(r'^requests$', api.v1.views.request),
-    url(r'^requests/(?P<request_id>\d+)/proposals$', api.v1.views.create_proposal),
+    url(r'^requests/(?P<request_id>\d+)/proposals$', api.v1.views.proposal),
     url(r'^requests/(?P<request_id>\d+)/proposals/(?P<proposal_id>\d+)$', api.v1.views.accept_proposal),
     url(r'^workers/(?P<worker_id>\d+)$', api.v1.views.show_worker),
-    url(r'^workers', api.v1.views.list_workers),
+    url(r'^request/(?P<request_id>\d+)/$', api.v1.views.list_proposals),
 ]
